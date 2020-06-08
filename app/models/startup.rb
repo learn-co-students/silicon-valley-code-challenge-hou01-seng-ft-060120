46accb56ea681171a8cab927d210d8b46cf616f5
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2020-06-08 17:15:23 -0500

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,84 @@
 class Startup
 
-end
+    @@all = []
+  
+    attr_reader :name, :founder, :domain
+  
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+  
+    def pivot(name, domain)
+        @name = name
+        @domain = domain
+    end
+  
+    def self.all
+        @@all 
+    end
+  
+    def self.find_by_founder(founder_name)
+        @@all.each do |startup|
+            if startup.founder == founder_name
+                return startup
+            end
+        end
+    end     
+  
+    def self.domains
+        domain_list = @@all.map {|startup| startup.domain}
+        domain_list.uniq
+    end
+
+    def sign_contract(venture_capitalist, type_of_investment, amount_invested)
+        FundingRound.new(self, venture_capitalist, type_of_investment, amount_invested)
+    end
+
+    def num_funding_rounds
+        funding_count = 0
+        FundingRound.all.each do |funding_round| 
+            if funding_round.startup == self
+                funding_count += 1
+            end
+        end
+        return funding_count
+    end
+
+    def total_funds
+        total_sum = 0
+        FundingRound.all.each do |funding_round|
+            if funding_round.startup == self
+                total_sum += funding_round.investment 
+            end
+        end
+        total_sum
+    end
+
+    def investors
+        investors = []
+        FundingRound.all.each do |funding_round|
+            if funding_round.startup == self
+                investors << funding_round.venture_capitalist
+            end
+        end
+        return investors.uniq
+    end
+
+    def big_investors
+        big_investors = []
+        FundingRound.all.each do |funding_round|
+            if funding_round.startup == self && funding_round.venture_capitalist.total_worth > 1000000000
+                big_investors << funding_round.venture_capitalist.name
+            end
+        end
+        big_investors.uniq
+    end
+  end
+  
+  amazon = Startup.new("amazon", "jeff", "ecommerce")
+  netflix = Startup.new("netflix", "reed", "video streaming")
+  hulu = Startup.new("hulu", "bob", "video streaming")
+  microsoft = Startup.new("microsoft", "bill", "computers")
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,38 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+
+## Startup Objects
+
+amazon = Startup.new("amazon", "jeff", "ecommerce")
+netflix = Startup.new("netflix", "reed", "video streaming")
+hulu = Startup.new("hulu", "bob", "video streaming")
+microsoft = Startup.new("microsoft", "bill", "computers")
+stripe = Startup.new("strip", "pat", "payments")
+blockchain = Startup.new("cypto", "john", "payments2")
+
+
+## VC Objects
+
+chase_capital = VentureCapitalist.new("chase", 1088888888888888888)
+f = VentureCapitalist.new("f", 1234587289317489237)
+z = VentureCapitalist.new("z", 1888888888888888)
+intel_capital = VentureCapitalist.new("intel", 10)
+bain_capital = VentureCapitalist.new("bain", 1000000001)
+x_capital = VentureCapitalist.new("x", 2394872904809)
+y_capital = VentureCapitalist.new("y", 555555555555)
+
+## Funding Rounds
+
+
+round1 = FundingRound.new(stripe, bain_capital, "Series A", 542233434543)
+round2 = FundingRound.new(hulu, chase_capital, "Angel", 100)
+round3 = FundingRound.new(hulu, intel_capital, "Series A", 10000000000023)
+round4 = FundingRound.new(microsoft, x_capital, "Series D", 2)
+round5 = FundingRound.new(hulu, z, "Series B", 23494039344882)
+round6 = FundingRound.new(microsoft, y_capital, "Series D", 29824332983298)
+round7 = FundingRound.new(netflix, f, "Angel", 2000000000000000)
+round8 = FundingRound.new(microsoft, chase_capital, "Angel", 2000000000000000)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
